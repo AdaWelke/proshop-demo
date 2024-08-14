2207c330a1ad92e16fb50444e2c0ff3791078a89
awelke <aaaa.welke@gmail.com>
2024-08-14 05:16:24 +0200

    got backend server.js running
    frontend uses backend server to fetch data

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,21 @@
 import { useParams, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
+	const [product, setProduct] = useState({});
 	const { id: productId } = useParams();
-	const product = products.find(p => p._id === productId);
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${productId}`);
+			setProduct(data);
+		};
+
+		fetchProduct();
+	}, [productId]);
 
 	return (
 		<>
